1527d0010d2ea723e0429f63a1f76878cd2cdb3e
djavolak <djavolak666@gmail.com>
2026-04-08 19:58:11 +0200

    css version bump

--- themes/admin/layout/crudTableLayout.php
+++ themes/admin/layout/crudTableLayout.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?=$this->e($pageTitle)?></title>
     <link rel="stylesheet" href="https://skeletor.greenfriends.systems/skeletorjs/css/style.css">
-    <link rel="stylesheet" href="<?=ADMIN_ASSET_URL . '/css/style.css'?>">
+    <link rel="stylesheet" href="<?=ADMIN_ASSET_URL . '/css/style.css?v=1'?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100;0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;0,9..40,900;0,9..40,1000;1,9..40,100;1,9..40,200;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600;1,9..40,700;1,9..40,800;1,9..40,900;1,9..40,1000&display=swap" rel="stylesheet">
